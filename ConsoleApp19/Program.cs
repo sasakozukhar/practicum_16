027ac6bafa277bc468db498a2c726fa75b1e7dda
sasakozukhar <sasakozuhar93@gmail.com>
2025-01-26 18:19:16 +0200

    Add files via upload

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 16; i++)
+            {
+                Console.Write("#");
+            }
+            for (int i = 0; i < 16 - 2; i++)
+            {
+                Console.Write("#");
+                for (int j = 0; j < 16 - 2; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("#");
+            }
+            for (int i = 0; i < 16; i++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+
+        
+    }
+}
