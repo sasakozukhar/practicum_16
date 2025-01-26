892d3492b2fc9db3615bba69ff4850457353b6c4
sasakozukhar <sasakozuhar93@gmail.com>
2025-01-26 11:35:50 +0200

    Add files via upload

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Printline1();
+
+            Console.WriteLine();
+            Printline2();
+
+            Console.WriteLine();
+            Printline3();
+
+            Console.WriteLine();
+            Printline4();
+            Console.Read();
+        }
+        static void Printline1()
+        {
+            for (int i = 41; i <= 50; i++)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.WriteLine();
+        }
+        static void Printline2()
+        {
+            for (int i = 51; i <= 60; i++)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.WriteLine();
+        }
+        static void Printline3()
+        {
+            for (int i = 61; i <= 70; i++)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.WriteLine();
+        }
+        static void Printline4()
+        {
+            for (int i = 71; i <= 80; i++)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
