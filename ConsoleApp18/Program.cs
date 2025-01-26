27f7f06dbb55b23f409df1d68754b267e9ac53e6
sasakozukhar <sasakozuhar93@gmail.com>
2025-01-26 18:05:49 +0200

    Add files via upload

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int rows = 5;
+
+    
+            for (int i = rows; i >= 1; i--)
+            {
+                
+                for (int j = 0; j < i; j++)
+                {
+                   
+                    Console.Write("1 ");
+                }
+
+               
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
